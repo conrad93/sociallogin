dadd19ddf19c53bfcf927c0c5355b7809d421a2f
Conrad Nunes <nunes.conrad93@gmail.com>
2020-08-23 15:18:08 +0530

    first commit

---
+++ logout.php
@@ -0,0 +1,17 @@
+<?php
+
+//logout.php
+
+include('config.php');
+
+//Reset OAuth access token
+// $google_client->revokeToken();
+$google_client->revokeToken($_SESSION['access_token']);
+
+//Destroy entire session data.
+session_destroy();
+
+//redirect page to index.php
+header('location:index.php');
+
+?>
