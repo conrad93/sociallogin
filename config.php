39ca3c3ad29e8c3f6ec672e198fbf0cd39d5fdbe
Conrad Nunes <nunes.conrad93@gmail.com>
2020-10-04 17:17:38 +0530

    second commit

--- config.php
+++ config.php
@@ -3,16 +3,16 @@
 session_start();
 require_once 'vendor/autoload.php';
 $google_client = new Google_Client();
-$google_client->setClientId('811481004870-qeo9ib86j6mg4f6vd4u87mi84laupmm2.apps.googleusercontent.com');
-$google_client->setClientSecret('Wua5fNbNrpHrZojoFjYVQYiX');
+$google_client->setClientId('insert your client id');
+$google_client->setClientSecret('insert your client secret');
 $google_client->setRedirectUri('http://localhost/sociallogin/gCallback.php');
 $google_client->addScope('email');
 $google_client->addScope('profile');
 
 require_once 'vendor/facebook/graph-sdk/src/Facebook/autoload.php';
 $facebook = new \Facebook\Facebook([
-'app_id' => '964233940741419',
-'app_secret' => 'b24a9061698a279c5e7f6ef007ac29ab',
+'app_id' => 'insert your id',
+'app_secret' => 'insert your client secret',
 'default_graph_version' => 'v8.0'
 ]);
 $facebook_helper = $facebook->getRedirectLoginHelper();
