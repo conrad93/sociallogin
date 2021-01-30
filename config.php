5be54217fee03bbfa8337ef5c06b5e9dcffb0c7e
conrad93 <64744613+conrad93@users.noreply.github.com>
2021-01-30 14:41:57 +0530

    Update config.php

--- config.php
+++ config.php
@@ -3,16 +3,16 @@
 session_start();
 require_once 'vendor/autoload.php';
 $google_client = new Google_Client();
-$google_client->setClientId('insert your client id here');
-$google_client->setClientSecret('insert your client secret here');
+$google_client->setClientId('insert your client id here'); // insert client id here from developer console
+$google_client->setClientSecret('insert your client secret here'); // insert client secret here from developer console
 $google_client->setRedirectUri('http://localhost/sociallogin/gCallback.php');
 $google_client->addScope('email');
 $google_client->addScope('profile');
 
 require_once 'vendor/facebook/graph-sdk/src/Facebook/autoload.php';
 $facebook = new \Facebook\Facebook([
-'app_id' => 'insert your id here',
-'app_secret' => 'insert your client secret here',
+'app_id' => 'insert your id here', // insert id here from developer console
+'app_secret' => 'insert your client secret here', // insert client secret here from developer console
 'default_graph_version' => 'v8.0' // check version on console.facebook.com
 ]);
 $facebook_helper = $facebook->getRedirectLoginHelper();
